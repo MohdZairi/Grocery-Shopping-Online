3c273209cc5d70a54ff972b97a8f9ce95f9cbae3
MohdZairi <mohdzairi658@gmail.com>
2022-10-08 18:27:24 +0800

    logout setup

--- dashboard.php
+++ dashboard.php
@@ -37,6 +37,7 @@
         <div class="fas fa-search" id="search-btn"></div>
         <div class="fas fa-shopping-cart" id="cart-btn"></div>
         <div class="fas fa-user" id="login-btn"></div>
+       
     </div>
 
     <form action="" class="search-form">
@@ -75,14 +76,8 @@
         <div class="total"> total : $19.69/- </div>
         <a href="#" class="btn">checkout</a>
     </div>
-
-    <form action="" class="login-form">
-        <h3>login now</h3>
-        <input type="email" placeholder="your email" class="box">
-        <input type="password" placeholder="your password" class="box">
-        <p>forget your password <a href="#">click here</a></p>
-        <p>don't have an account <a href="register.php">create now</a></p>
-        <input type="submit" value="login now" class="btn">
+    <form action="logout.php" class="login-form">
+        <a href="logout.php"><input type="submit" value="logout now" class="btn"></a>
     </form>
 
 </header>
---
+++ logout.php
@@ -0,0 +1,18 @@
+<?php 
+include "inc/config.php";
+session_start();
+session_unset();
+session_destroy();
+
+$current_time = time();
+$current_date = date("Y-m-d H:i:s", $current_time);
+
+// Set Cookie expiration for 1 month
+$cookie_expiration_time = $current_time - (30*24*60*60);  
+
+setcookie('member_login', $row['ID'],$cookie_expiration_time);
+setcookie('random_password', $random_password,$cookie_expiration_time);
+setcookie('random_selector', $random_selector,$cookie_expiration_time);
+
+
+header("Location: index.php");
